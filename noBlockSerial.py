4547d0a0249b9bec72aaef9f6bf414d7b4c9a9c3
Safi Amir <46702275+safinwb@users.noreply.github.com>
2021-06-30 22:56:58 +0400

    Python - Non Threaded

---
+++ noBlockSerial.py
@@ -0,0 +1,25 @@
+#Non Blocking Serial Code for Python
+
+import serial
+import time
+
+ser = serial.Serial('COM5', baudrate = 115200)
+
+data = [0]
+
+print("""\
+                    __  _____           _       __
+   __  _____  ___  / /_/ ___/___  _____(_)___ _/ /
+  / / / / _ \/ _ \/ __/\__ \/ _ \/ ___/ / __ `/ / 
+ / /_/ /  __/  __/ /_ ___/ /  __/ /  / / /_/ / /  
+ \__, /\___/\___/\__//____/\___/_/  /_/\__,_/_/   
+/____/                                            
+    """)
+
+print("Send command to listed Serial:")
+
+while (True):
+    if (ser.in_waiting):
+        buf = ser.read(ser.in_waiting)
+        print(buf)
+        time.sleep(0.01)
